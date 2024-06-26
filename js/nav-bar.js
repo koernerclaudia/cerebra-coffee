f230f055956c6cc515d4ec7c78b2083b67a7b651
Claudia Koerner <claudia.koerner@gmail.com>
2024-06-26 22:12:54 +0200

    Update of code

--- js/nav-bar.js
+++ js/nav-bar.js
@@ -1,18 +1,3 @@
-$('.dropdown').click(function(e){
-    e.stopPropagation();
-    $('.nav-dropdown').toggle();
+$('.handle').on('click', function() {
+    $('nav ul').toggleClass('showing')
   });
-  
-  $('html').click(function(){
-    $('.nav-dropdown').hide();
-  })
-  
-  $('#nav-toggle').on('click', function() {
-    this.classList.toggle('active');
-  });
-  
-  $("#nav-toggle").click(function() {
-        $("nav ul").toggle();
-      });
-      
-      
