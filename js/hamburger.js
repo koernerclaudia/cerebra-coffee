d2b0a5dd323aa6da6168d79728b276ea217596eb
Claudia Koerner <claudia.koerner@gmail.com>
2024-06-29 17:11:43 +0200

    Updates done

--- js/hamburger.js
+++ js/hamburger.js
@@ -10,4 +10,5 @@ document.querySelectorAll(".nav-link").forEach(n => n.
     addEventListener("click", () => {
         hamburger.classList.remove("active");
         navMenu.classList.remove("active");
-    }))
+    })
+)
