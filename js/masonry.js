26ba1c49031cb06720a6eb2bec2dd0aba0fda931
Claudia Koerner <claudia.koerner@gmail.com>
2024-07-07 23:54:46 +0200

    Deleted tryout for media 500px

--- js/masonry.js
+++ js/masonry.js
@@ -1,14 +1,16 @@
-let MagicGrid;
-
 document.addEventListener('DOMContentLoaded', function () {
     let cerebraMenu = new MagicGrid({
-      container: '.container',
-      animate: true,
-      items: 16,
-      gutter: 30,
-      static: true,
-      useMin: true
+        container: '.container',
+        animate: true,
+        items: 16,
+        gutter: 30,
+        maxColumns: 3,
+        useTransform: true,
+        static: true,
+        useMin: true
     });
     
     cerebraMenu.listen();
-  });
+
+
+});
