fdd1481313b4422b24c06e6f72bdc8c8b27cf4be
Claudia Koerner <claudia.koerner@gmail.com>
2024-07-07 22:51:49 +0200

    Updated menu page
    I added a masonry grid for the menu that has javascript.
    
    https://codepen.io/chriscoyier/pen/KbrmKG
    
    https://github.com/e-oj/Magic-Grid

---
+++ js/masonry.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let cerebraMenu = new MagicGrid({
+      container: '.container',
+      animate: true,
+      items: 16,
+      gutter: 30,
+      static: true,
+      useMin: true
+    });
+  
+    cerebraMenu.listen();
+  });
